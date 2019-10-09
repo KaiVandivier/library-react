1751c0eb3d81b53f327188f37623cd55c803bdf3
Kai Vandivier <kpvandivier@gmail.com>
2019-10-09 13:08:08 +0200

    Updates

---
+++ precache-manifest.e18c1c59c80c4bd523267ee49bbfba10.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c146dfe9ce0465325509d472a2a7e497",
+    "url": "/index.html"
+  },
+  {
+    "revision": "913f8ea3eae4f1a36de6",
+    "url": "/static/css/main.55339ce6.chunk.css"
+  },
+  {
+    "revision": "fc25c19f77376b69f1b8",
+    "url": "/static/js/2.6b3dd4c4.chunk.js"
+  },
+  {
+    "revision": "913f8ea3eae4f1a36de6",
+    "url": "/static/js/main.700346be.chunk.js"
+  },
+  {
+    "revision": "7c47a8067e63918ce9ff",
+    "url": "/static/js/runtime-main.8a8a76b1.js"
+  }
+]);
